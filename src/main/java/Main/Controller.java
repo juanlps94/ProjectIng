aa62853731260a4d5c04a2e44298270525b43e65
Juan <juanluisperez001@gmail.com>
2023-05-19 19:53:39 -0400

    project start

---
+++ src/main/java/Main/Controller.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+/**
+ *
+ * @author Juan
+ */
+public class Controller {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+}
