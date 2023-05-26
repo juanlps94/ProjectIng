064294f9998dbad5bd82a5f5d844db12dc97459a
Juan <juanluisperez001@gmail.com>
2023-05-26 19:01:58 -0400

    Merge origin/master

--- src/main/java/Main/Controller.java
+++ src/main/java/Main/Controller.java
@@ -1,18 +1,37 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package Main;
+
+import Model.*;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import view.*;
 
 /**
  *
  * @author Juan
  */
 public class Controller {
+    ArrayList<Usuario> arrUsuarios = new ArrayList<Usuario>();
+    ArrayList<Administrador> arrAdministrador = new ArrayList<Administrador>();
+    
+    //variables de las interfaces, singlenton y otros.
+    public V_Inicio vistaInicio = new V_Inicio();
+   
 
+    //Constructor
+    public Controller() {
+    }
+
+    public void setV() {
+        this.vistaInicio.setVisible(true);
+    }
+
+    //Constructor
     public static void main(String[] args) {
-
-        
-
+        Controller principal = new Controller();
+        principal.setV();
     }
 }
